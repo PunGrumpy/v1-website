2c783fe1a828cebca03bbccddc1723f58239a958
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-14 01:21:07 +0700

    feat(metadata): add metadata

---
+++ app/works/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: {
+    default: 'Works',
+    template: '%s | Works'
+  },
+  description: 'A collection of my works'
+}
+
+export default function WorksLayout({ children }) {
+  return <>{children}</>
+}
